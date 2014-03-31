27a264dd6f0d1329b4677dd5fb7d626812daa709
jialong <jialong@admobius.com>
2014-03-30 18:24:42 -0700

    Fixed script path

--- start_win_svc.js
+++ start_win_svc.js
@@ -4,7 +4,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
   name:'Home Automation Control Center',
   description: 'Home automation control centor nodejs web service',
-  script: 'C:\\Users\\jialo g\\Documents\\GitHub\\control_center\\index.js'
+  script: 'C:\\Users\\jialong\\Documents\\GitHub\\control_center\\index.js'
 });
 
 // Listen for the "install" event, which indicates the
