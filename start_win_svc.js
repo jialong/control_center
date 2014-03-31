55192cb620d3909316d7534a649b9c2824b60eb8
jialong <jialong@admobius.com>
2014-03-30 18:23:32 -0700

    script to add web service as windows service and start the service

---
+++ start_win_svc.js
@@ -0,0 +1,16 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'Home Automation Control Center',
+  description: 'Home automation control centor nodejs web service',
+  script: 'C:\\Users\\jialo g\\Documents\\GitHub\\control_center\\index.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
